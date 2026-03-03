d7592499652eb9787d862d6d64e402ad352a0700
Your Name <you@example.com>
2026-03-03 10:34:16 -0700

    testing model

--- app/home/index.php
+++ app/home/index.php
@@ -2,6 +2,6 @@
 
 Use App\Models\Core\Propflyer;
 
-$flyers= Propflyer::query()->orderBy('created_at', 'desc')->get();
+$flyers= Propflyer::query()->orderBy('created_at', 'desc')->limit(50)->get();
 
 dd($flyers);
