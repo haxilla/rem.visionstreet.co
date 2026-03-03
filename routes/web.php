f03bd6bdb34eeace4ed01358b5a13bad64e3e5b6
Your Name <you@example.com>
2026-03-03 09:44:58 -0700

    testing model

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     require app_path('code/users_rebuild.php');
-    require app_path('home/index.php');    // require throws fatal if file has parse error / etc.
+    //require app_path('home/index.php');    // require throws fatal if file has parse error / etc.
     return view('index');
 });
