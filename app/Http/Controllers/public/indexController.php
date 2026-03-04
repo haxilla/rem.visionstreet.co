5c35c7ad99ac756cb25df224c44bb91e4a819a0e
Your Name <you@example.com>
2026-03-04 11:51:28 -0700

    adding public.index controller etc.

---
+++ app/Http/Controllers/public/indexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\public;
+use App\Http\Controllers\Controller;
+
+use Request;
+
+class indexController extends Controller
+{
+
+  public function index(){
+
+    require app_path('code/users_rebuild.php');
+    require app_path('public/index.php');
+
+    //return view
+    return view('mdbxPublic.index',
+      [
+        'newAdds'     => $newAdds,
+        'mostViews'   => $mostViews,
+      ]);
+  }
+
+}
