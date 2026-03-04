165406a17d87e1a7035279d3c9307db4bf56c114
Your Name <you@example.com>
2026-03-04 06:18:07 -0700

    fixing models

--- app/Models/Core/Propagentmeta.php
+++ app/Models/Core/Propagentmeta.php
@@ -4,7 +4,7 @@ namespace App\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Propagentmeta extends \App\Model
+class Propagentmeta extends Model
 {
    protected $primaryKey   = 'propagent_id';
    protected $table        = 'remuserdb.propagentmetas';
--- app/Models/Core/Propflyer.php
+++ app/Models/Core/Propflyer.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models\core;
+namespace App\Models\Core;
 
 use Illuminate\Database\Eloquent\Model;
 
