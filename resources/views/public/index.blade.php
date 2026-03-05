ca92eebf68b301f9ff1e0554bc66dd8a2597420d
Your Name <you@example.com>
2026-03-04 18:03:19 -0700

    new swiper slide

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -20,60 +20,60 @@
     <div class="swiper" data-swiper>
       <div class="swiper-wrapper">
         @foreach($newAdds as $the)
-          <div class="swiper-slide">
-            <div class="responsiveImgContainer" style="position:relative;">
-              <img class="d-block w-100 responsiveImg"
-              src="https://www.realtyrepublic.com/hqphotos/{{$the->theMeta->zipDir}}/{{$the->theMeta
-                ->mlsDir}}/{{$the->thePhotos->where('def','=','1')
-                ->first()->photoName}}"
-                alt="{{$the->xFullStreet}} Main">
-            </div>
+<div class="swiper-slide relative h-[420px]">
+
+    <!-- Background listing photo -->
+    <img
+        class="absolute inset-0 w-full h-full object-cover"
+        src="https://www.realtyrepublic.com/hqphotos/{{$the->theMeta->zipDir}}/{{$the->theMeta->mlsDir}}/{{$the->thePhotos->where('def','=','1')->first()->photoName}}"
+        alt="{{$the->xFullStreet}}"
+    >
+
+    <!-- optional dark gradient overlay -->
+    <div class="absolute inset-0 bg-gradient-to-t from-black/70 via-black/20 to-transparent"></div>
+
+    <!-- CONTENT OVERLAY -->
+    <div class="absolute bottom-0 left-0 right-0 p-6 text-white">
+
+        <div class="text-sm uppercase tracking-wide mb-1">
+            Featured
+        </div>
+
+        <div class="text-xl font-semibold">
+            {{$the->xFullStreet}}
+        </div>
+
+        <div class="text-sm opacity-90">
+            {{$the->xCity}}, {{$the->xState}} {{$the->xxZip}}
+        </div>
+
+        <!-- Agent -->
+        <div class="flex items-center gap-3 mt-4">
+
+            <img
+                class="w-12 h-12 rounded-full object-cover border-2 border-white"
+                src='https://www.realtyrepublic.com/agentPhotos/{{$the->theAgent->theAgentCleanup->newRemID ?? ""}}/{{$the->theAgent->agtPhoto}}'
+            >
+
             <div>
-              <div>
-                Featured
-              </div>
-              <div>
-                {{$the->xFullStreet}}
-              </div>
-              <div>
-                <div>
-                  {{$the->xCity}}, {{$the->xState}} {{$the->xxZip}}
+                <div class="font-semibold">
+                    {{$the->theAgent->agtFullName}}
                 </div>
-              </div>
-            </div>
-            <div class="carouselAgentWrapper">
-              <div>
-                <div style="display:table-cell;">
-                  @if($the->theAgent->agtPhoto)
-                    <div style="display:inline-block;vertical-align:bottom;">
-                      @if($the->theAgent->theAgentCleanup)
-                        <img class="carouselAgentImg"
-                        src='https://www.realtyrepublic.com/agentPhotos/{{$the->theAgent->theAgentCleanup
-                          ->newRemID}}/{{$the->theAgent->agtPhoto}}'>
-                      @else
-                        <img class="carouselAgentImg"
-                        src='https://realtyemails.com/HQoffice/{{$the->theOffice
-                          ->officeID}}/{{$the->theAgent->agtPhoto}}'>
-                      @endif
-                    </div>
-                  @endif
-                  <div style="display:inline-block;vertical-align:bottom;">
-                    <div>
-                      <div class="carouselAgentName">
-                        {{$the->theAgent->agtFullName}}
-                      </div>
-                      <div class="carouselAgentCompany">
-                        {{$the->theOffice->officeName}}
-                      </div>
-                      <div class="carouselAgentPhone">
-                        {{$the->theAgent->agtMainPhone}}
-                      </div>
-                    </div>
-                  </div>
+
+                <div class="text-sm opacity-90">
+                    {{$the->theOffice->officeName}}
                 </div>
-              </div>
+
+                <div class="text-sm opacity-90">
+                    {{$the->theAgent->agtMainPhone}}
+                </div>
             </div>
-          </div>
+
+        </div>
+
+    </div>
+
+</div>
         @endforeach
         <!-- END LOOP 1 -->
       </div>
@@ -82,5 +82,5 @@
       <div class="swiper-button-next"></div>
     </div>
   </body>
-  
+
 </html>
