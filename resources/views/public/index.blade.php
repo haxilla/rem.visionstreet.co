cb696c4a42fdc9886ce7228726d77b889a97f75b
Your Name <you@example.com>
2026-03-06 14:12:49 -0700

    top viewed 2

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -85,7 +85,7 @@
                             <div class="grid grid-cols-1 md:grid-cols-[220px_1fr] gap-5 md:gap-6 items-start">
 
                                 {{-- Larger photo --}}
-                                <div class="overflow-hidden rounded-[22px] bg-[#e8e8ec] h-[160px] sm:h-[180px] md:h-[170px] lg:h-[185px]">
+                                <div class="overflow-hidden rounded-[15px] bg-[#e8e8ec] h-[165px] sm:h-[185px] md:h-[175px] lg:h-[190px]">
                                     @if($listingImg)
                                         <img
                                             src="{{ $listingImg }}"
