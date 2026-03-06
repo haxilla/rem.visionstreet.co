b3d5c49cee191d1273343653723f1f9c3984ee42
Your Name <you@example.com>
2026-03-06 16:22:20 -0700

    moasic trial one

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -14,36 +14,53 @@
   <section class="bg-[#f5f5f7] py-12 lg:py-16">
     @include('public.includes.top_views_3')
   </section>
-  <section>Member Since Count:{{$memberSince->count()}}</section>
+  <section>
 
+  @php
+    $tilePattern = [
+        'tile-lg-portrait',
+        'tile-square',
+        'tile-square',
+        'tile-square',
+        'tile-portrait',
+        'tile-wide',
+        'tile-square',
+        'tile-square',
+        'tile-portrait',
+        'tile-square',
+        'tile-square',
+        'tile-lg-portrait',
+    ];
+@endphp
 
-  <div class="agent-mosaic-luxury">
+<div class="agent-mosaic-luxury">
+    @foreach($memberSince as $index => $agent)
+        @php
+            $tileClass = $tilePattern[$index % count($tilePattern)];
 
-    <div class="agent-tile tile-lg-portrait">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
+            $folder = $agent->theAgentMeta->newRemID
+                ?? $agent->theAgentCleanup->newRemID
+                ?? null;
 
-    <div class="agent-tile tile-square">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
+            $photo = ($folder && !empty($agent->agtPhoto))
+                ? "https://realtyrepublic.com/agentPhotos/{$folder}/{$agent->agtPhoto}"
+                : null;
+        @endphp
 
-    <div class="agent-tile tile-square">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
-
-    <div class="agent-tile tile-square">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
-
-    <div class="agent-tile tile-portrait">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
-
-    <div class="agent-tile tile-wide">
-        <img src="https://realtyrepublic.com/agentPhotos/8oz9nhtjjq/5980agtphoto-FCC4DDADA96F69C94C4C2A0D5FDCB626.jpg">
-    </div>
-
+        @if($photo)
+            <div class="agent-tile {{ $tileClass }}">
+                <img
+                    src="{{ $photo }}"
+                    alt="{{ $agent->agtFullName }}"
+                    loading="lazy"
+                    decoding="async"
+                >
+            </div>
+        @endif
+    @endforeach
 </div>
+
+  </section>
 
 
 </body>
