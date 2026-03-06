c24d996961a469434f9e8debb7c7c2b96299e50c
Your Name <you@example.com>
2026-03-06 16:17:22 -0700

    member since count check

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -14,6 +14,7 @@
   <section class="bg-[#f5f5f7] py-12 lg:py-16">
     @include('public.includes.top_views_3')
   </section>
+  <section>Member Since Count:{{$memberSince->count()}}</section>
 
 
   <div class="agent-mosaic-luxury">
