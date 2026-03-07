e42e6d5ef761235c567333c9f9d59f2f70174197
Your Name <you@example.com>
2026-03-07 11:48:53 -0700

    style

--- resources/views/public/includes/top_views_4up_v3.blade.php
+++ resources/views/public/includes/top_views_4up_v3.blade.php
@@ -139,8 +139,18 @@
 
             <aside class="flex flex-col items-start gap-7">
 
+                                    {{-- flyer visual --}}
+                        <div class="mt-5 flex justify-center">
+                            <img
+                                src="{{ asset('images/2FlyerPerspective.gif') }}"
+                                alt="Flyer preview"
+                                class="h-auto w-auto max-w-[210px] sm:max-w-[240px] object-contain rotate-[7deg] drop-shadow-[0_18px_24px_rgba(0,0,0,.28)]"
+                            >
+                        </div>
+
                 {{-- FREE FLYER SIDEBAR --}}
                 <div class="w-full rounded-[30px] bg-[#1e3566] p-7 sm:p-8 shadow-[0_20px_55px_rgba(0,0,0,.22)]">
+
 
                     <div class="flex flex-col items-center text-center">
 
@@ -150,15 +160,6 @@
                             style="border-color: {{ $brandGold }}; background: rgba(255,255,255,.06);"
                         >
                             <i class="ti-wand text-[22px]" style="color:#f0d28a;"></i>
-                        </div>
-
-                        {{-- flyer visual --}}
-                        <div class="mt-5 flex justify-center">
-                            <img
-                                src="{{ asset('images/2FlyerPerspective.gif') }}"
-                                alt="Flyer preview"
-                                class="h-auto w-auto max-w-[210px] sm:max-w-[240px] object-contain rotate-[7deg] drop-shadow-[0_18px_24px_rgba(0,0,0,.28)]"
-                            >
                         </div>
 
                         {{-- label --}}
