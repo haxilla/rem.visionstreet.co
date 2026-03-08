0b15a35113b2f56f5b719b9b422fc66efcc77b5f
Your Name <you@example.com>
2026-03-07 18:14:17 -0700

    style

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -12,7 +12,7 @@
         @include('public.includes.features_section')
     </section>
     <section>
-        @include('wide_99_promo')
+        @include('public.includes.wide_99_promo')
     </section>
     <section >
         @include('public.includes.top_views_4up_v1')
