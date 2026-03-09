155f2b8127c4bdfe6d3cedb888c7c6f3a8f7a098
Your Name <you@example.com>
2026-03-08 19:34:44 -0700

    style

--- resources/views/public/includes/features_section.blade.php
+++ resources/views/public/includes/features_section.blade.php
@@ -46,7 +46,7 @@ $brandBlue    = $brandBlue ?? '#214e9b';
 $dividerColor = $dividerColor ?? 'bg-[#e6ebf6]';
 @endphp
 
-<div class="mx-auto w-full max-w-screen-2xl px-4 sm:px-6 lg:px-10 pt-4 lg:pt-6" style="max-width:1600px;">
+<div class="mx-auto w-full max-w-screen-2xl px-4 sm:px-6 lg:px-10 pt-4 lg:pt-6" style="max-width:1400px;">
 
     {{-- Header --}}
     <div class="text-center">
