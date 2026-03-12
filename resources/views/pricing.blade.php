47cf9bbe5a80531536849b0c3efa23054ae0b74d
Your Name <you@example.com>
2026-03-12 11:34:43 -0700

    route logic

--- resources/views/pricing.blade.php
+++ resources/views/pricing.blade.php
@@ -1,1 +1,18 @@
-This is the pricing page
+@include('public.layout.head')
+
+<body data-section="admin" 
+class="linkcheck relative bg-white min-h-screen font-sans text-gray-800 postgres">
+
+  @include('public.layout.nav')
+
+  <main class="transition-all duration-300 min-h-screen pt-24 relative"
+  :class="collapsed ? 'ml-20' : 'ml-64'">
+    <div class="ml-8 mr-8 lg:ml-10 lg:mr-10">
+      <div class="pageswap p-6 w-full">
+        THIS IS THE PRICING PAGE
+      </div>
+    </div>
+  </main>
+  @include('globals.footer.admin')
+</body>
+</html>
