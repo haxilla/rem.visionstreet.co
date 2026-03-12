d98689ffdac96e90cb4bdbd50a74cf66a8f8d2ad
Your Name <you@example.com>
2026-03-12 11:38:21 -0700

    type

--- app/Http/Controllers/guest/guestController.php
+++ app/Http/Controllers/guest/guestController.php
@@ -103,7 +103,7 @@ class guestController extends Controller
 
         // full chrome + the same fragment on refresh/direct visit
         return response()
-            ->view($viewname, [         
+            ->view($viewName, [         
                 'data'        => $data,
                 'contentView' => $viewName,
             ])->header('Vary', 'X-Pageswap');
