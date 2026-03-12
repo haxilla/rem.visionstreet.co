696fedffba83bdf26d1129e67b3d5c18cb4b389c
Your Name <you@example.com>
2026-03-12 11:57:32 -0700

    pages created

---
+++ resources/views/demo.blade.php
@@ -0,0 +1,18 @@
+@include('public.layout.head')
+
+<body data-section="admin" 
+class="linkcheck relative bg-white min-h-screen font-sans text-gray-800 postgres">
+
+  @include('public.layout.nav')
+
+  <main class="transition-all duration-300 min-h-screen pt-24 relative"
+  :class="collapsed ? 'ml-20' : 'ml-64'">
+    <div class="ml-8 mr-8 lg:ml-10 lg:mr-10">
+      <div class="pageswap p-6 w-full">
+        THIS IS THE DEMO PAGE
+      </div>
+    </div>
+  </main>
+  @include('public.layout.footer')
+</body>
+</html>
