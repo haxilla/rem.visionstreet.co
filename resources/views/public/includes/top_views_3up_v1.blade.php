35dd25bf13a91012b2642008aeb045477f6f08c4
Your Name <you@example.com>
2026-03-13 14:59:27 -0700

    most viewed links

--- resources/views/public/includes/top_views_3up_v1.blade.php
+++ resources/views/public/includes/top_views_3up_v1.blade.php
@@ -47,6 +47,8 @@
                         $listingImg = "https://realtyrepublic.com/hqphotos/{$the->theMeta->zipDir}/{$the->theMeta->mlsDir}/{$photo}";
                     }
 
+                    $listingURL="https://realtyrepublic.com/homedetails/{$the->url_slug}
+
                     $agentImg = null;
                     if (!empty($the->theAgent?->agtPhoto) && !empty($the->theAgent?->theAgentCleanup?->newRemID)) {
                         $agentImg = "https://realtyrepublic.com/agentPhotos/{$the->theAgent->theAgentCleanup->newRemID}/{$the->theAgent->agtPhoto}";
@@ -67,11 +69,13 @@
                     {{-- Photo --}}
                     <div class="h-[230px] overflow-hidden rounded-[18px] bg-[#e8e8ec]">
                         @if($listingImg)
-                            <img
-                                src="{{ $listingImg }}"
-                                alt="{{ $street }}"
-                                class="h-full w-full object-cover transition duration-500 group-hover:scale-[1.035]"
-                            >
+                            <a href="{{ $listingURL }}" target="_blank">
+                                <img
+                                    src="{{ $listingImg }}"
+                                    alt="{{ $street }}"
+                                    class="h-full w-full object-cover transition duration-500 group-hover:scale-[1.035]"
+                                >
+                            </a>
                         @endif
                     </div>
 
