fa555705678138bca04837e0f167873acd45796c
Your Name <you@example.com>
2026-03-15 10:51:01 -0700

    admin login form in progress

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,5 +1,18 @@
+@include('public.layout.head')
 
+<body data-section="admin" 
+class="linkcheck relative bg-white min-h-screen font-sans text-gray-800 postgres">
 
-<div>
-    LOGIN FORM GOES HERE
-</div>
+  @include('public.layout.nav')
+
+  <main class="transition-all duration-300 min-h-screen pt-24 relative"
+  :class="collapsed ? 'ml-20' : 'ml-64'">
+    <div class="ml-8 mr-8 lg:ml-10 lg:mr-10">
+      <div class="pageswap p-6 w-full">
+        LOGIN FORM GOES HERE
+      </div>
+    </div>
+  </main>
+  @include('public.layout.footer')
+</body>
+</html>
