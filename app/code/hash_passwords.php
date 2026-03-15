12ce1e51b097a0ae87abb205269ae45bccc824de
Your Name <you@example.com>
2026-03-15 14:30:10 -0700

    member login logic

--- app/code/hash_passwords.php
+++ app/code/hash_passwords.php
@@ -1,11 +1,11 @@
 <?php
 
 $users = \App\Models\User::whereNull('password')
-    ->whereNotNull('plain_password')
-    ->where('plain_password', '!=', '')
+    ->whereNotNull('password_plain')
+    ->where('password_plain', '!=', '')
     ->get();
 
 foreach ($users as $user) {
-    $user->password = \Hash::make($user->plain_password);
+    $user->password = \Hash::make($user->password_plain);
     $user->save();
 }
