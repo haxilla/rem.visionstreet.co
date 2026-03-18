382fb4852ee2926964b4150ffeabdc1ca015d23e
Your Name <you@example.com>
2026-03-18 13:59:22 -0700

    moved variables to separate flyer

--- app/flyers/variables.php
+++ app/flyers/variables.php
@@ -1,3 +1,21 @@
 <?php
 
 include('countBullets.php');
+
+$fromURL='https://www.realtyrepublic.com';
+$display="screen";
+$enc=0;
+
+$totalPhotos = $propInfo->thePhotos
+->where('resized','=','500')
+->count();
+
+$graphic_words      = $propInfo->theStyle->graphic_words;
+$graphic_textcolor  = $propInfo->theStyle->graphic_textcolor;
+$graphic_style      = $propInfo->theStyle->graphic_style;
+$hlGraphic          = $graphic_words.'_'.$graphic_textcolor.'_'.$graphic_style.'x.png';
+
+//theHeadline
+$theHeadline=$propInfo['xHeadline'];
+if(!$theHeadline){
+    $theHeadline=$propInfo['xxHeadline'];}
--- resources/views/flyers/index.blade.php
+++ resources/views/flyers/index.blade.php
@@ -1,26 +1,6 @@
 @php 
-    $fromURL='https://www.realtyrepublic.com';
-    $display="screen";
-    $enc=0;
-
-    $totalPhotos = $propInfo->thePhotos
-    ->where('resized','=','500')
-    ->count();
-
-    $graphic_words      = $propInfo->theStyle->graphic_words;
-    $graphic_textcolor  = $propInfo->theStyle->graphic_textcolor;
-    $graphic_style      = $propInfo->theStyle->graphic_style;
-    $hlGraphic          = $graphic_words.'_'.$graphic_textcolor.'_'.$graphic_style.'x.png';
-
-    //theHeadline
-    $theHeadline=$propInfo['xHeadline'];
-    if(!$theHeadline){
-        $theHeadline=$propInfo['xxHeadline'];}
-
     include(app_path().'/flyers/variables.php');
-        
 @endphp
-
 
 <div>
     YOURE ON THE FLYERS INDEX PAGE
