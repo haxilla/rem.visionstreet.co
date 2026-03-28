1f5625a8e78cf844921280bdda3fae92c20b244a
Your Name <you@example.com>
2026-03-27 20:18:03 -0700

    adding colors

--- resources/views/flyers/index.blade.php
+++ resources/views/flyers/index.blade.php
@@ -159,6 +159,193 @@ class="linkcheck relative bg-white min-h-screen font-sans text-gray-800 postgres
             });
         </script>
     </div>
+
+    <div class="fixed-top pb-2 z-depth-1 pt-1 editColorsTopNav"
+style="background-color:rgba(255,255,255,0.9)">
+    <div class="container row" style="padding-left:0;padding-right:0;">
+        <div class="col-lg-1 col-md-1 col-sm-1 col-1">
+            <div class="colorsLargeIcon pl-1">
+                <i class="mt-2 fa fa-2x fa-paint-brush">
+                </i>
+            </div>
+            <div class="colorsSmallIcon pl-1">
+                <i class="mt-2 fa fa-paint-brush">
+                </i>
+            </div>
+        </div>
+        <div class="col-xl-7 col-lg-7 col-md-9 col-sm-9 col-10"
+        style="padding-left:1rem;padding-right:0;">
+            <div class="text-dark2" >
+                <div>
+                    <div>
+                        <div class="colorTitle">
+                            Background
+                        </div>
+                        <div id="background_swatches" style="display:inline-block;">
+                         <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/eeeeee"
+                         style="text-decoration:none;"
+                         class="colorswatch">
+                           <img src="/images/color_schemes/swatch_eeeeee.gif"
+                             width="20"
+                             height="20"
+                             border="1" />
+                         </a>
+                        <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/cccccc"
+                        style="text-decoration:none;"
+                        class="colorswatch">
+                            <img src="/images/color_schemes/swatch_cccccc.gif"
+                            width="20"
+                            height="20"
+                            border="1" />
+                        </a>
+                        @if($theTemplate == 's1pc')
+                            <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/999999"
+                            style="text-decoration:none;"
+                            class="colorswatch">
+                                <img src="/images/color_schemes/swatch_999999.gif"
+                                width="20"
+                                height="20"
+                                border="1" />
+                            </a>
+                        @endif
+                        <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/000066"
+                        style="text-decoration:none;"
+                        class="colorswatch">
+                            <img src="/images/color_schemes/swatch_006.gif"
+                            width="20"
+                            height="20"
+                            border="1" />
+                        </a>
+                        <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/996600"
+                        style="text-decoration:none;"
+                        class="colorswatch">
+                            <img src="/images/color_schemes/swatch_996600.gif"
+                            width="20"
+                            height="20"
+                            border="1" />
+                        </a>
+                        <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/990000"
+                        style="text-decoration:none;"
+                        class="colorswatch">
+                            <img src="/images/color_schemes/swatch_990000.gif"
+                            width="20"
+                            height="20"
+                            border="1" />
+                        </a>
+                        <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/b/000000"
+                        style="text-decoration:none;"
+                        class="colorswatch">
+                            <img src="/images/color_schemes/swatch_000000.gif"
+                             width="20"
+                             height="20"
+                             border="1" />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-dark2">
+            <div class="colorTitle">
+                Accents
+            </div>
+            <div id="text_swatches" style="display:inline-block;">
+                <div class="minColors lightColors">
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/ffffff"
+                    style="text-decoration:none;"
+                    class="colorswatch noLight">
+                        <img src="/images/color_schemes/swatch_ffffff.gif"
+                        width="20"
+                        height="20"
+                        border="1"
+                        style="border:1px solid #cccccc;"/>
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/eeeeee"
+                    style="text-decoration:none;"
+                    class="colorswatch noLight">
+                        <img src="/images/color_schemes/swatch_eeeeee.gif"
+                         width="20"
+                         height="20"
+                         border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/ffffcc"
+                    style="text-decoration:none;"
+                    class="colorswatch noLight">
+                        <img src="/images/color_schemes/swatch_ffffcc.gif"
+                        width="20"
+                        height="20"
+                        border="1" />
+                    </a>
+                </div>
+                <div style="display:inline-block;" class="darkColors">
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/ffc60b"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_ffc60b.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/990000"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_990000.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/000066"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_006.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/00aeef"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_00aeef.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/60b67b"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_60b67b.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/f0535b"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_f0535b.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                    <a href="/mdbxMember/colorChoice/{{$propInfo->theMeta->sk1}}/t/ff0000"
+                    style="text-decoration:none;"
+                    class="colorswatch noDark">
+                     <img src="/images/color_schemes/swatch_ff0000.gif"
+                       width="20"
+                       height="20"
+                       border="1" />
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-1 col-md-1 col-sm-1 col-1"
+    style="padding-left:0;padding-right:0;">
+        <div class="colorsCompleteButton menuCompleteButton z-depth-1 hoverable mr-5">
+            <i title="Click when finished"
+            class="fa fa-check"></i>
+        </div>
+    </div>
+</div>
+
   </main>
   @include('public.layout.footer')
 </body>
