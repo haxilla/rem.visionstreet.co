c896a6713da875ae06a8fd4e77e5092d7da0c618
Your Name <you@example.com>
2026-04-13 15:30:32 -0700

    flyer_slug

--- app/Http/Controllers/guest/guestController.php
+++ app/Http/Controllers/guest/guestController.php
@@ -82,7 +82,7 @@ class guestController extends Controller
         require app_path('code/flyer_states.php');
         require app_path('code/flyer_codes.php');
         //require app_path('code/hash_passwords.php');
-        //require app_path('code/flyer_slug.php');
+        require app_path('code/flyer_slug.php');
         require app_path('public/index.php');
 
         //return view
