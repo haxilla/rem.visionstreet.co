1140751a315233462ea0789613f562af8b37bcb1
Your Name <you@example.com>
2026-04-29 14:04:21 -0700

    headline.js

--- public/my/js/flyers/headline.js
+++ public/my/js/flyers/headline.js
@@ -2,6 +2,12 @@ document.getElementById('headlineSelect').addEventListener('change', function ()
     const selected = this.value;
 
     document.querySelectorAll('.hlGraphic').forEach(img => {
-        img.src = `https://www.realtyrepublic.com/images/headline_graphics/${selected}/ul/${selected}_ffc60b_ulx.png`;
+        const currentSrc = img.src;
+
+        // extract color between _ and _
+        const match = currentSrc.match(/_([0-9a-fA-F]{6})_/);
+        const color = match ? match[1] : 'ffffff'; // fallback if not found
+
+        img.src = `https://www.realtyrepublic.com/images/headline_graphics/${selected}/ul/${selected}_${color}_ulx.png`;
     });
 });
