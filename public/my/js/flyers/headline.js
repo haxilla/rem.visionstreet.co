331fc674df44abac9c67103eb4bc555ba90b90f9
Your Name <you@example.com>
2026-04-29 19:05:10 -0700

    headline style

--- public/my/js/flyers/headline.js
+++ public/my/js/flyers/headline.js
@@ -11,3 +11,22 @@ document.getElementById('headlineSelect').addEventListener('change', function ()
         img.src = `https://www.realtyrepublic.com/images/headline_graphics/${selected}/ul/${selected}_${color}_ulx.png`;
     });
 });
+
+document.getElementById('headlineStyle').addEventListener('change', function () {
+    const newStyle = this.value;
+
+    document.querySelectorAll('.hlGraphic').forEach(img => {
+        const currentSrc = img.src;
+
+        // extract headline (folder + filename base)
+        const nameMatch = currentSrc.match(/headline_graphics\/([^/]+)\//);
+        const name = nameMatch ? nameMatch[1] : '';
+
+        // extract color
+        const colorMatch = currentSrc.match(/_([0-9a-fA-F]{6})_/);
+        const color = colorMatch ? colorMatch[1] : 'ffffff';
+
+        img.src = `https://www.realtyrepublic.com/images/headline_graphics/${name}/${newStyle}/${name}_${color}_${newStyle}x.png`;
+    });
+});
+
