1adf8bcac8d9921842ffe329e1dfdde01747afc1
Your Name <you@example.com>
2026-05-07 21:38:43 -0700

    agentphoto

--- resources/views/flyers/flyerParts/agtContactBanner.blade.php
+++ resources/views/flyers/flyerParts/agtContactBanner.blade.php
@@ -31,7 +31,7 @@ font-family:arial;">
           class="agentImage"
             style="display:block;
             max-width:100%;
-            max-height:90px;
+            max-height:125px;
             padding-left:15px;">
         @endif
       </div>
