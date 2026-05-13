55f3872fe17905ce7fb2a8c238ece0c41fb9182d
Your Name <you@example.com>
2026-05-13 15:11:18 -0700

    agent delete

--- app/admin/agentDelete.php
+++ app/admin/agentDelete.php
@@ -3,32 +3,28 @@
 use App\Models\Core\Propagent;
 use Illuminate\Support\Facades\DB;
 
-public function agentDelete($id)
-{
-    $agent = Propagent::findOrFail($id);
+$agent = Propagent::findOrFail($id);
 
-    // Safety check
-    if ($agent->startDate) {
-        return redirect()->back();
-    }
-
-    /*
-    |--------------------------------------------------------------------------
-    | Delete from remote server/database
-    |--------------------------------------------------------------------------
-    */
-
-    DB::connection('remote_realtyemails')
-        ->table('remailagents')
-        ->where('umid', $agent->id)
-        ->delete();
-
-    /*
-    |--------------------------------------------------------------------------
-    | Delete local record
-    |--------------------------------------------------------------------------
-    */
-
-    $agent->delete();
-
+// Safety check
+if ($agent->startDate) {
+    return redirect()->back();
 }
+
+/*
+|--------------------------------------------------------------------------
+| Delete from remote server/database
+|--------------------------------------------------------------------------
+*/
+
+DB::connection('remote_realtyemails')
+    ->table('remailagents')
+    ->where('umid', $agent->id)
+    ->delete();
+
+/*
+|--------------------------------------------------------------------------
+| Delete local record
+|--------------------------------------------------------------------------
+*/
+
+$agent->delete();
