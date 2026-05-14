428cfc419fcec76626a5225457fa4c7ac0f9d3a8
Your Name <you@example.com>
2026-05-14 13:17:11 -0700

    adding member dashboard login for admin

--- app/Http/Controllers/admin/adminController.php
+++ app/Http/Controllers/admin/adminController.php
@@ -62,10 +62,16 @@ class adminController extends Controller
 
     public function agentLogin($id)
     {
+        $member = Member::findOrFail($id);
 
-        include(app_path().'/admin/agent/login.php');
-        return redirect()->back();
+        session([
+            'impersonator_admin_id' => Auth::guard('admin')->id(),
+            'impersonating_member_id' => $member->id,
+        ]);
 
+        Auth::guard('member')->login($member);
+
+        return redirect()->route('/member/dashboard');
     }
 
 }
