403c6e14cd41fec0e9139cc697316f45a54f261e
Your Name <you@example.com>
2026-05-14 20:11:32 -0700

    the stats

--- app/member/dashboard.php
+++ app/member/dashboard.php
@@ -6,7 +6,7 @@ Use Illuminate\Support\Facades\Auth;
 
 $agentID=Auth::guard('member')->id();
 
-$propflyers = Propflyer::with('theAgent')->where('propagent_id', $agentID)->get();
+$propflyers = Propflyer::with(['theAgent','theStats'])->where('propagent_id', $agentID)->get();
 $propdelivs = Propdelivnow::with([
     'theFlyer.thePhotos' => function ($query) {
         $query->where('def', 1);
