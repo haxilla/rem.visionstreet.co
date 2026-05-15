325e8693f6f2ba02d0e10024fe3ad2fb692012d0
Your Name <you@example.com>
2026-05-14 19:10:10 -0700

    the flyer with photos

--- app/member/dashboard.php
+++ app/member/dashboard.php
@@ -7,6 +7,6 @@ Use Illuminate\Support\Facades\Auth;
 $agentID=Auth::guard('member')->id();
 
 $propflyers = Propflyer::with('theAgent')->where('propagent_id', $agentID)->get();
-$propdelivs = Propdelivnow::with('theFlyer')->where('propagent_id', $agentID)->get();
+$propdelivs = Propdelivnow::with('theFlyer.thePhotos')->where('propagent_id', $agentID)->get();
 
 dd($propflyers, $propdelivs);
