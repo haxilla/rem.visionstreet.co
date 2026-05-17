8269e9496ae3b66019bf99b71ab44a2f845735a6
Your Name <you@example.com>
2026-05-17 13:17:14 -0700

    bouncecount

--- app/admin/bounces/index.php
+++ app/admin/bounces/index.php
@@ -1,21 +1,17 @@
 <?php
 
-dd(phpinfo());
-
-/*
 $mailbox = imap_open(
         '{mail.realtye-mails.com:110/pop3/notls}INBOX',
         'members@realtye-mails.com',
         'PASSWORD'
     );
 
-    if (!$mailbox) {
-        return imap_last_error();
-    }
+if (!$mailbox) {
+    return imap_last_error();
+}
 
-    $count = imap_num_msg($mailbox);
+$count = imap_num_msg($mailbox);
 
-    imap_close($mailbox);
+imap_close($mailbox);
 
-    return "Connected successfully. Message count: " . $count;
-*/
+dd("Connected successfully. Message count: " . $count);
