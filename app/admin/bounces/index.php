4dd017e4b371e00dc51c3434b884ef16a92437c0
Your Name <you@example.com>
2026-05-17 13:22:47 -0700

    bounce check

--- app/admin/bounces/index.php
+++ app/admin/bounces/index.php
@@ -1,13 +1,19 @@
 <?php
 
-$mailbox = imap_open(
-    '{mail.realtye-mails.com:110/pop3/notls/novalidate-cert}INBOX',
+$mailboxPath = '{mail.realtye-mails.com:110/pop3/notls}INBOX';
+
+$mailbox = @imap_open(
+    $mailboxPath,
     'members@realtye-mails.com',
-    'D4vidb0wi3!'
+    'PASSWORD'
 );
 
 if (!$mailbox) {
-    dd(imap_errors(), imap_last_error());
+    dd([
+        'mailboxPath' => $mailboxPath,
+        'errors' => imap_errors(),
+        'last_error' => imap_last_error(),
+    ]);
 }
 
 $count = imap_num_msg($mailbox);
