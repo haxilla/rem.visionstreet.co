ea00ffaa27b36b0cccec551628fc98ba82711614
Your Name <you@example.com>
2026-05-17 13:10:25 -0700

    bounce connection test

--- app/admin/bounces/index.php
+++ app/admin/bounces/index.php
@@ -1,3 +1,17 @@
 <?php
 
-dd("This is the page for bounce data)");
+$mailbox = imap_open(
+        '{mail.realtye-mails.com:110/pop3/notls}INBOX',
+        'members@realtye-mails.com',
+        'PASSWORD'
+    );
+
+    if (!$mailbox) {
+        return imap_last_error();
+    }
+
+    $count = imap_num_msg($mailbox);
+
+    imap_close($mailbox);
+
+    return "Connected successfully. Message count: " . $count;
