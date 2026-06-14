6cefd63b9ee8294d85b619561ae3d4cf31084b1c
Your Name <you@example.com>
2026-06-13 20:15:18 -0700

    flyer editor

--- resources/views/flyers/index.blade.php
+++ resources/views/flyers/index.blade.php
@@ -7,13 +7,13 @@ class="linkcheck relative bg-white min-h-screen font-sans text-gray-800 postgres
 
   <main class="transition-all duration-300 min-h-screen pt-24 relative"
   :class="collapsed ? 'ml-20' : 'ml-64'">
-    <div class="ml-8 mr-8 lg:ml-10 lg:mr-10">
-        <div class="pageswap p-6 w-full">
+    <div class="mx-3 lg:mx-10">
+        <div class="pageswap p-0 lg:p-6 w-full">
             @php 
                 include(app_path().'/flyers/variables.php');
             @endphp
 
-            <div class="mb-6 border border-gray-200 rounded-xl bg-white shadow-sm overflow-hidden"
+            <div class="mb-4 border border-gray-200 rounded-xl bg-white shadow-sm overflow-hidden"
             style="max-width:600px;">
                 {{-- Header --}}
                 <div class="px-4 pt-4 bg-gray-50 border-b border-gray-200">
