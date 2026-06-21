199c74bd06cd86b5225f93a362e57236cd220346
Your Name <you@example.com>
2026-06-21 11:22:12 -0700

    xZip

--- app/member/flyer/save.php
+++ app/member/flyer/save.php
@@ -5,13 +5,9 @@ $validatedData = $request->validate([
     'xFullStreet' => 'required|string|max:255',
     'xCity' => 'required|string|max:100',
     'xState' => 'required|string|max:2',
-    'xZip' => 'required|string|max:10',
+    'xZip' => 'required|digits:5',
     // Add other fields as necessary
 ]);
 
-echo "BEFORE REDIRECT<br>";
-
 redirect('/member/flyer/details')->send();
-
-echo "AFTER REDIRECT";
 exit();
