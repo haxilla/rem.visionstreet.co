8b7dd5c8b3585cff4ae4268802daf32a2864d735
Your Name <you@example.com>
2026-06-21 11:07:54 -0700

    removed validation

--- app/member/flyer/save.php
+++ app/member/flyer/save.php
@@ -1,21 +1,6 @@
 <?php
 
-// Validate the request data
-$validatedData = $request->validate([
-    'address' => 'required|string|max:255',
-    'city' => 'required|string|max:100',
-    'state' => 'required|string|max:2',
-    'zip' => 'required|string|max:10',
-    // Add other fields as necessary
-]);
 
-dd($validatedData);
-
-if ($validator->fails()) {
-
-    dd($validator->errors()->toArray());
-
-}
 
 echo "BEFORE REDIRECT<br>";
 
