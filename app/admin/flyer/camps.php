dc32b5c00d1bc568348f3e5d3d8687410c4ca63b
Your Name <you@example.com>
2026-06-20 21:00:42 -0700

    flyer camps

--- app/admin/flyer/camps.php
+++ app/admin/flyer/camps.php
@@ -3,6 +3,15 @@
 include app_path('queries/campaigns.php');
 include app_path('queries/flyerdetails.php');
 
+$emailCounts = [
+    'azphxmetro' => DB::connection('rememaildb')->table('azphxmetro')->count(),
+    'azphxne'    => DB::connection('rememaildb')->table('azphxne')->count(),
+    'azphxse'    => DB::connection('rememaildb')->table('azphxse')->count(),
+    'azphxwv'    => DB::connection('rememaildb')->table('azphxwv')->count(),
+    'aznaz'      => DB::connection('rememaildb')->table('aznaz')->count(),
+    'azsaz'      => DB::connection('rememaildb')->table('azsaz')->count(),
+];
+
 $data = [
     'waitingFlyerCamps'    => $waitingFlyerCamps,
     'inProgressFlyerCamps' => $inProgressFlyerCamps,
