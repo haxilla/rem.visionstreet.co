e126a8ea10cc7821ad3996b73df760e252cb654f
Your Name <you@example.com>
2026-06-21 18:26:13 -0700

    vite test

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
             input: [
                 //'resources/css/app.css',
                 'resources/js/app.js',
-                //'resources/js/photo-modal.js',
+                'resources/js/photo-modal.js',
             ],
             refresh: true,
         }),
