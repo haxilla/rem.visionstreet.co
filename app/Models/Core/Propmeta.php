00d6542c0cebca38559bd2ddc163d58de6450817
Your Name <you@example.com>
2026-06-21 21:58:51 -0700

    propmeta fix

--- app/Models/Core/Propmeta.php
+++ app/Models/Core/Propmeta.php
@@ -10,4 +10,10 @@ class Propmeta extends Model
    protected $primaryKey   = 'propflyer_id';
    protected $table        = 'remuserdb.propmetas';
 
+   protected $fillable = [
+    'propflyer_id',
+    'zipDir',
+    'mlsDir',
+   ];
+
 }
