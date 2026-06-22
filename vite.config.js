33b57850066893669e019ef2138277c216f50d4f
Your Name <you@example.com>
2026-06-21 18:28:59 -0700

    vite fix

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                //'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/photo-modal.js',
+                'resources/css/app.css',
+                //'resources/js/app.js',
+                //'resources/js/photo-modal.js',
             ],
             refresh: true,
         }),
