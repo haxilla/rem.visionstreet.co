4657ab1356ff07f6999d473602802c76f2274355
Your Name <you@example.com>
2026-06-23 06:05:30 -0700

    property details

--- resources/views/public/details.blade.php
+++ resources/views/public/details.blade.php
@@ -181,7 +181,7 @@
             </div>
 
             <div class="mt-5">
-                Property Summary
+                <u>Property Summary</u>
             </div>
 
             {{-- Facts --}}
