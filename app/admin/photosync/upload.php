50e87d1adcbd930158b2b19e8b901b5cacd20c67
Your Name <you@example.com>
2026-06-27 22:02:26 -0700

    coldfusion upload

--- app/admin/photosync/upload.php
+++ app/admin/photosync/upload.php
@@ -8,6 +8,17 @@ try {
         echo "❌ Local file disappeared: {$photo->photoName}<br>";
         return false;}
 
+    $response = Http::attach(...)
+    ->post(...);
+
+    echo "HTTP Status: " . $response->status() . "<br><br>";
+
+    echo "<pre>";
+    echo htmlentities($response->body());
+    echo "</pre>";
+
+    die();
+
     $response = Http::timeout(30)
         ->attach(
             'photo',
@@ -16,7 +27,7 @@ try {
         )
         ->post('https://realtyemails.com/photosync/upload.cfm', [
 
-            'secret' => '6R3vN9!xAq27M#LpZ84fGh@1sWbTcY',
+            'secret' => '5db2d7b8f4c74d5abcc42fd3e9183c44',
             'zipDir' => $photo->theMeta->zipDir,
             'mlsDir' => $photo->theMeta->mlsDir,
 
