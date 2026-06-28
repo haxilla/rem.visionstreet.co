4f4582602e244fb549e92e65366d937d022d1527
Your Name <you@example.com>
2026-06-28 16:51:18 -0700

    resize work

--- app/admin/photosync/resize.php
+++ app/admin/photosync/resize.php
@@ -33,9 +33,19 @@ foreach ($photos as $photo) {
 
         continue;}
 
+    //sets ratio, orient, height, width
     include('getDimensions.php');
 
-    include('checkResize.php');
+    // if large enough, then resize
+    if(($orient=='wide' && $width>1000)
+    ||($orient=='tall' && $height>600)){
+        //include resize script
+        include('resize1000.php');
+
+    }else{
+        //include update only 
+        include('updatePhoto.php');}
+        
 
 }
 
