3b1515fe88fc8c0e0cbf74a98caf670f3eec57cf
Your Name <you@example.com>
2026-06-27 20:56:21 -0700

    zipDir/mlsDir

--- app/admin/photosync/index.php
+++ app/admin/photosync/index.php
@@ -2,11 +2,13 @@
 
 Use App\Models\Core\Propphoto;
 
-$photos = Propphoto::with('theMeta')
-    ->whereDate('photoDate', '>=', '2026-05-01')
-    ->orderBy('propflyer_id')
-    ->orderBy('ord')
-    ->take(10)
-    ->get();
+$photos = Propphoto::with([
+    'theMeta' => function ($query) {
+        $query->select('propflyer_id', 'zipDir', 'mlsDir');
+    }
+])
+->whereDate('photoDate', '>=', '2026-05-01')
+->take(10)
+->get();
 
 dd($photos);
