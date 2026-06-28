1f6b67c7dc2202885ca3d83d90bed5c996b9019b
Your Name <you@example.com>
2026-06-27 20:50:41 -0700

    propphoto start

--- app/admin/photosync/index.php
+++ app/admin/photosync/index.php
@@ -1,3 +1,11 @@
 <?php
 
-dd("PHOTOSYNC INDEX");
+Use App\Models\Core\Propphoto;
+
+$photos = Propphoto::with('theMeta')
+    ->whereDate('photoDate', '>=', '2026-05-01')
+    ->orderBy('propflyer_id')
+    ->orderBy('photoOrder')
+    ->get();
+
+dd($photos);
