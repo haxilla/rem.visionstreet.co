f22ba669505fcd7beadcd00e05d851f40c45dd2f
Your Name <you@example.com>
2026-06-28 16:08:43 -0700

    resize starting

--- app/admin/photosync/resize.php
+++ app/admin/photosync/resize.php
@@ -1,5 +1,19 @@
 <?php
 
+use App\Models\Core\Propphoto;
+
+$batchSize = 1;
+
+$total = Propphoto::whereDate('photoDate','>=','2026-05-01')
+    ->where('resized',0)
+    ->count();
+
+$remaining = Propphoto::whereDate('photoDate','>=','2026-05-01')
+    ->where('resized',0)
+    ->count();
+
+$processed = $total - $remaining;
+
 header('Content-Type: application/json');
 
 echo json_encode([
