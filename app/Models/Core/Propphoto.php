0305d1695a52c1a6bf52eb5c4738b28df15a7603
Your Name <you@example.com>
2026-06-27 20:53:42 -0700

    propphoto model

--- app/Models/Core/Propphoto.php
+++ app/Models/Core/Propphoto.php
@@ -8,4 +8,9 @@ class Propphoto extends Model
 {
    protected $primaryKey = 'photoID';
    protected $dates = ['existCheck','photoDate'];
+
+   public function theMeta(){
+      return $this->hasOne('App\Models\Core\Propmeta','propflyer_id','propflyer_id');
+   }
+
 }
