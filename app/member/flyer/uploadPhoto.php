c17f20d173c634daeace4e9df40d14e1599fff64
Your Name <you@example.com>
2026-07-03 11:40:00 -0700

    photo upload step

--- app/member/flyer/uploadPhoto.php
+++ app/member/flyer/uploadPhoto.php
@@ -2,9 +2,24 @@
 
 header('Content-Type: application/json');
 
+if (!isset($_FILES['photo'])) {
+
+    echo json_encode([
+        'success' => false,
+        'message' => 'No photo received'
+    ]);
+
+    exit;
+
+}
+
 echo json_encode([
     'success' => true,
-    'message' => 'Upload endpoint reached'
+    'message' => 'Photo received',
+    'name'    => $_FILES['photo']['name'],
+    'size'    => $_FILES['photo']['size'],
+    'type'    => $_FILES['photo']['type'],
+    'error'   => $_FILES['photo']['error']
 ]);
 
 exit;
